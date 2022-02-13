b97b46225bf0581265d83eab61e9c36fc168c68e
Edgars Kalējs <36110349+Ediny@users.noreply.github.com>
2022-02-13 22:25:39 +0200

    Delete createButton.php

--- PHP/createButton.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once './controler.php';
-$stmt = "SELECT SUBSTRING_INDEX(email_name, '@', -1) domain COUNT(*) AS domain FROM email";
-
-
-
-echo '<input type="submit" value="<?php echo $button; ?>" name="<?php echo $button; ?>" form="main-form">';
